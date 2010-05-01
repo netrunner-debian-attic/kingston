cfafeceebba889064b86ffc54cfcee2e7402d271
Sune Vuorela <sune@vuorela.dk>
2010-05-01 21:54:34 +0200

    launch a update check on launch

--- src/kingston_update_notifier_module.cpp
+++ src/kingston_update_notifier_module.cpp
@@ -41,6 +41,7 @@ kingston_update_notifier_module_t::kingston_update_notifier_module_t(QObject* pa
     m_worker(new worker_t(this)), m_listener(new listener_t(this)), m_notifier(new notifier_t(this)) {
   connect(m_listener,SIGNAL(please_check_for_updates()),m_worker,SLOT(check_for_updates()));
   connect(m_worker,SIGNAL(updates_available(int,int)),m_notifier,SLOT(notify_new_updates(int,int)));
+  m_worker->check_for_updates();
 }
 
 
